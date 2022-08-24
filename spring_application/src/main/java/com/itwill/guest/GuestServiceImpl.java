9d573ed9a6e9e2260c8fd4e2b1ea56cf5d36f147
academyitwill <jhyun9682@naver.com>
2022-08-24 16:22:57 +0900

    ..

---
+++ spring_application/src/main/java/com/itwill/guest/GuestServiceImpl.java
@@ -0,0 +1,50 @@
+package com.itwill.guest;
+
+import java.util.List;
+
+public class GuestServiceImpl implements GuestService {
+	
+	private GuestDao guestDao;
+	
+	public GuestServiceImpl() {
+		System.out.println("1.#### GuestServiceImpl() 기본생성자");
+	}
+	public GuestServiceImpl(GuestDao guestDao) {
+		System.out.println("1.#### GuestServiceImpl("+guestDao+") 생성자");
+		this.guestDao=guestDao;
+	}
+	
+	public void setGuestDao(GuestDao guestDao) {
+		System.out.println("#### GuestServiceImpl : setGuestDao("+guestDao+") 호출");
+		this.guestDao = guestDao;
+	}
+
+
+
+	@Override
+	public List<Guest> selectAll()throws Exception{
+		System.out.println("#### GuestServiceImpl : selectAll() 호출");
+		return  guestDao.selectAll();
+	}
+	@Override
+	public Guest selectByNo(int no)throws Exception{
+		System.out.println("#### GuestServiceImpl : selectByNo(int no) 호출");
+		return guestDao.selectByNo(no);
+	}
+	@Override
+	public int insertGuest(Guest guest)throws Exception{
+		System.out.println("#### GuestServiceImpl : insertGuest(Guest guest) 호출");
+		return 0;
+	}
+	@Override
+	public int updateGuest(Guest guest)throws Exception{
+		System.out.println("#### GuestServiceImpl : updateGuest(Guest guest) 호출");
+		return 0;
+	}
+	@Override
+	public int deleteGuest(int no)throws Exception{
+		System.out.println("#### GuestServiceImpl : deleteGuest(int no) 호출");
+		return 0;
+	}
+	
+}
---
+++ spring_application/src/main/java/com/itwill/user/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.itwill.user;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+	private UserDao userDao;
+	public UserServiceImpl() {
+		System.out.println("#### UserServiceImpl() : 디폴트생성자호출");
+	}
+	
+	public void setUserDao(UserDao userDao) {
+		System.out.println("#### UserServiceImpl : setUserDao("+userDao+") 호출");
+		this.userDao = userDao;
+	}
+	public int create(User user) throws Exception {
+		System.out.println("#### UserServiceImpl : create() 호출");
+		return 0;
+	}
+	public User findUser(String userId) throws Exception {
+		System.out.println("#### UserServiceImpl : findUser() 호출");
+		return null;
+	}
+	public int login(String userId, String password) throws Exception {
+		System.out.println("#### UserServiceImpl : login() 호출");
+		return 0;
+	}
+	public int update(User user) throws Exception {
+		System.out.println("#### UserServiceImpl : update() 호출");
+		return 0;
+	}
+	public int remove(String userId) throws Exception {
+		System.out.println("#### UserServiceImpl : remove() 호출");
+		return 0;
+	}
+	public List<User> findUserList() throws Exception {
+		System.out.println("#### UserServiceImpl : findUserList() 호출  ");
+		return null;
+	}
+}
